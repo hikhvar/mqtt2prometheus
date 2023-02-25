bc2bcd8edc9f0eb1b3098dfb0dd3ea89b77b6903
Christoph Petrausch <263448+hikhvar@users.noreply.github.com>
2023-02-25 21:11:20 +0100

    Update renovate.js

--- .github/renovate.js
+++ .github/renovate.js
@@ -1,27 +1,25 @@
 module.exports = {
-    branchPrefix: 'test-renovate/',
-    dryRun: false,
-    username: 'renovate-release',
-    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    onboarding: false,
-    platform: 'github',
-    includeForks: false,
-    repositories: [
-        'hikhvar/mqtt2prometheus',
-    ],
-    packageRules: [
-        {
-            description: 'lockFileMaintenance',
-            matchUpdateTypes: [
-                'pin',
-                'digest',
-                'patch',
-                'minor',
-                'major',
-                'lockFileMaintenance',
-            ],
-            dependencyDashboardApproval: false,
-            stabilityDays: 10,
-        },
-    ],
+ "branchPrefix": "test-renovate/",
+ "dryRun": null,
+ "username": "renovate-release",
+ "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
+ "onboarding": false,
+ "platform": "github",
+ "includeForks": false,
+ "repositories": ["hikhvar/mqtt2prometheus"],
+ "packageRules": [
+   {
+     "description": "lockFileMaintenance",
+     "matchUpdateTypes": [
+       "pin",
+       "digest",
+       "patch",
+       "minor",
+       "major",
+       "lockFileMaintenance"
+     ],
+     "dependencyDashboardApproval": false,
+     "stabilityDays": 10
+   }
+ ]
 };
